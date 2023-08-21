f4909eb80107c89b050d91f5b1fe6d3477571e5f
HikiMan <tech.denisov@gmail.com>
2023-08-21 18:01:56 +0400

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let str_n = String(n);
+    if (str_n[0] == '-') {
+        n = Number(str_n.substring(1));
+    }
+    while (Math.abs(n > 0.99)) {
+        result += Math.floor(n % 10);
+        n /= 10;
+    }
+    return Number(result);
 }
